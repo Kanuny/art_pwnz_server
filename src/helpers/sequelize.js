0d3a65e1445dae7bc15bc640d47f2d76642c9079
kanuny <andrey.kanunnikov@wookieelabs.com>
2017-03-05 16:00:04 +0200

    back to dev settings

--- src/helpers/sequelize.js
+++ src/helpers/sequelize.js
@@ -1,14 +1,12 @@
-
 // @flow
 import Sequelize from 'sequelize';
 
 // FIXME remove default values
-const userName = process.env.DB_USER || 'root';
-const password = process.env.DB_PASSWORD || '1';
-const dbName = process.env.DB_NAME || 'test_art_pwnz';
 
-const DB = new Sequelize(dbName, userName, password, {
-  dialect: 'mysql',
+const dbName = 'postgres://pqikqlbdiibqtm:446ac5fde8f58617178909624e4efb89b1e6c462a3497eea33f0865267bea569@ec2-54-217-235-11.eu-west-1.compute.amazonaws.com:5432/d2dehklu264u6n';
+
+const DB = new Sequelize(dbName, {
+  dialect: 'postgres',
   define: {
     timestamps: false,
   },
