51a46e935bba27f2a051eadec6300b1c7dac8670
kanuny <andrey.kanunnikov@wookieelabs.com>
2017-03-08 22:57:32 +0200

    add auth routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,6 +6,7 @@ import article from './article';
 import history from './history';
 import video from './video';
 import image from './image';
+import auth from './auth';
 
 const router = koaRouter();
 
@@ -14,4 +15,5 @@ export default compose(
   history,
   video,
   image,
+  auth,
 )(router);
