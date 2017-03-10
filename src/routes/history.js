3ee7cb12674a7794309563e7517957562364e514
kanuny <andrey.kanunnikov@wookieelabs.com>
2017-03-10 09:04:03 +0200

    remove auth check from images

--- src/routes/history.js
+++ src/routes/history.js
@@ -3,7 +3,6 @@ import Article from '../models/Article';
 import Image from '../models/Image';
 import Video from '../models/Video';
 import Localization from '../models/Localization';
-import auth from '../middlewares/auth';
 
 export default (router: any) => {
   router.get('/history', async (ctx, next) => {
--- src/routes/image.js
+++ src/routes/image.js
@@ -1,10 +1,9 @@
 // @flow
 import Image from '../models/Image';
 import { getPreview } from '../helpers/images';
-import auth from '../middlewares/auth';
 
 export default (router: any) => {
-  router.get('/images/:id', auth(['admin']), async (ctx, next) => {
+  router.get('/images/:id', async (ctx, next) => {
     const { id } = ctx.params;
     const image = await Image.findById(id);
 
