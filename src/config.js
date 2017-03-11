551262cdd392b1f5c03746c3232d98f44157e152
kanuny <andrey.kanunnikov@wookieelabs.com>
2017-03-11 21:34:15 +0200

    change email address

--- src/config.js
+++ src/config.js
@@ -2,5 +2,5 @@ export default {
   port: process.env.PORT || 3000,
   pageSize: process.env.PAGE_SIZE || 12,
   MAIL_KEY: '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',
-  MAIL_ADDRESS: 'andreykanuny@gmail.com',
+  MAIL_ADDRESS: 'frtwork@gmail.com',
 };
