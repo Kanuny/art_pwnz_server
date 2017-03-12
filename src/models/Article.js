013a27299e89fd0e9a82de1794f2bed8b9292723
kanuny <andrey.kanunnikov@wookieelabs.com>
2017-03-12 09:26:50 +0200

    adjust sharing

--- src/models/Article.js
+++ src/models/Article.js
@@ -10,7 +10,9 @@ const Article = sequelize.define('article', {
   hidden: BOOLEAN,
   removed: BOOLEAN,
   createdAt: DATE,
+  size: STRING,
   year: STRING,
+  genre: STRING,
   forSale: BOOLEAN,
 }, {
   timestamps: false
